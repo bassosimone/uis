18a1187a131fdd24289e989a6a3dc68b9c45a7c9
Simone Basso <bassosimone@gmail.com>
2026-01-02 14:24:27 +0100

    feat: write tests for VNIC.InjectFrame

--- vnic_test.go
+++ vnic_test.go
@@ -3,6 +3,7 @@
 package uis_test
 
 import (
+	"sync/atomic"
 	"testing"
 
 	"github.com/bassosimone/uis"
@@ -22,6 +23,18 @@ func (vnicDispatcher) DeliverNetworkPacket(tcpip.NetworkProtocolNumber, *stack.P
 
 func (vnicDispatcher) DeliverLinkPacket(tcpip.NetworkProtocolNumber, *stack.PacketBuffer) {
 	// nothing
+}
+
+type countingDispatcher struct {
+	count atomic.Uint32
+}
+
+func (d *countingDispatcher) DeliverNetworkPacket(tcpip.NetworkProtocolNumber, *stack.PacketBuffer) {
+	d.count.Add(1)
+}
+
+func (d *countingDispatcher) DeliverLinkPacket(tcpip.NetworkProtocolNumber, *stack.PacketBuffer) {
+	d.count.Add(1)
 }
 
 func TestVNICInterfaceMethods(t *testing.T) {
@@ -51,4 +64,41 @@ func TestVNICInterfaceMethods(t *testing.T) {
 	assert.False(t, vnic.IsAttached())
 
 	require.NotPanics(t, vnic.Wait)
+}
+
+func TestVNICInjectFrameDiscardCases(t *testing.T) {
+	t.Run("zero_length", func(t *testing.T) {
+		vnic := uis.NewVNIC(uis.MTUEthernet, nil)
+		assert.False(t, vnic.InjectFrame(uis.VNICFrame{}))
+	})
+
+	t.Run("unknown_protocol", func(t *testing.T) {
+		vnic := uis.NewVNIC(uis.MTUEthernet, nil)
+		disp := &countingDispatcher{}
+		vnic.Attach(disp)
+		assert.False(t, vnic.InjectFrame(uis.VNICFrame{Packet: []byte{0x70}}))
+		assert.Zero(t, disp.count.Load())
+	})
+
+	t.Run("closed", func(t *testing.T) {
+		vnic := uis.NewVNIC(uis.MTUEthernet, nil)
+		disp := &countingDispatcher{}
+		vnic.Attach(disp)
+		vnic.Close()
+		assert.False(t, vnic.InjectFrame(uis.VNICFrame{Packet: []byte{0x40}}))
+		assert.Zero(t, disp.count.Load())
+	})
+
+	t.Run("no_dispatcher", func(t *testing.T) {
+		vnic := uis.NewVNIC(uis.MTUEthernet, nil)
+		assert.False(t, vnic.InjectFrame(uis.VNICFrame{Packet: []byte{0x40}}))
+	})
+
+	t.Run("larger_than_mtu", func(t *testing.T) {
+		vnic := uis.NewVNIC(1, nil)
+		disp := &countingDispatcher{}
+		vnic.Attach(disp)
+		assert.False(t, vnic.InjectFrame(uis.VNICFrame{Packet: []byte{0x40, 0x00}}))
+		assert.Zero(t, disp.count.Load())
+	})
 }
