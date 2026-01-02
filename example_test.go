4690fea24be78c5ba0e6924b8722081a17f42c6a
Simone Basso <bassosimone@gmail.com>
2026-01-02 10:02:22 +0100

    feat: add UDP example

--- example_test.go
+++ example_test.go
@@ -15,7 +15,7 @@ import (
 // This example creates a client and the server and the client
 // downloads a small number of bytes from the server.
 func Example_clientDownload() {
-	// create the internet and router instance.
+	// create the internet instance.
 	ix := uis.NewInternet(uis.InternetOptionMaxInflight(256))
 
 	// create the server and client stacks
@@ -80,5 +80,77 @@ loop:
 
 	// Output:
 	// Hello, world!
+	//
+}
+
+// This example creates a client and server using UDP over IPv4.
+// The server echoes back whatever it receives.
+func Example_udpEchoIPv4() {
+	// create the internet instance.
+	ix := uis.NewInternet(uis.InternetOptionMaxInflight(256))
+
+	// create the server and client stacks
+	const mtu = uis.MTUJumbo
+	srv := runtimex.PanicOnError1(ix.NewStack(mtu, netip.MustParseAddr("10.0.0.1")))
+	defer srv.Close()
+
+	clnt := runtimex.PanicOnError1(ix.NewStack(mtu, netip.MustParseAddr("10.0.0.2")))
+	defer clnt.Close()
+
+	// create a context used by connector and listener
+	ctx := context.Background()
+
+	// run the server in the background
+	wg := &sync.WaitGroup{}
+	ready := make(chan struct{})
+	wg.Go(func() {
+		listenCfg := uis.NewListenConfig(srv)
+		pconn := runtimex.PanicOnError1(listenCfg.ListenPacket(ctx, "udp", "10.0.0.1:53"))
+		defer pconn.Close()
+		close(ready)
+		buffer := make([]byte, 2048)
+		count, addr := runtimex.PanicOnError2(pconn.ReadFrom(buffer))
+		_ = runtimex.PanicOnError1(pconn.WriteTo(buffer[:count], addr))
+	})
+
+	// run the client in the background
+	messagech := make(chan []byte, 1)
+	wg.Go(func() {
+		<-ready
+		connector := uis.NewConnector(clnt)
+		conn := runtimex.PanicOnError1(connector.DialContext(ctx, "udp", "10.0.0.1:53"))
+		message := []byte("Hello, IPv4!\n")
+		_ = runtimex.PanicOnError1(conn.Write(message))
+		buffer := make([]byte, 1024)
+		count := runtimex.PanicOnError1(conn.Read(buffer))
+		messagech <- buffer[:count]
+		runtimex.PanicOnError0(conn.Close())
+	})
+
+	// know when both goroutines have stopped
+	stopped := make(chan struct{})
+	go func() {
+		wg.Wait()
+		close(stopped)
+	}()
+
+	// route packets in the foreground
+loop:
+	for {
+		select {
+		case frame := <-ix.InFlight():
+			// Here is where you'll potentially mess with packets
+			_ = ix.Deliver(frame)
+		case <-stopped:
+			break loop
+		}
+	}
+
+	// receive and print the echoed message
+	message := <-messagech
+	fmt.Printf("%s", string(message))
+
+	// Output:
+	// Hello, IPv4!
 	//
 }
